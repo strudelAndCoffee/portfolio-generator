00bb7d77371771f502f6ac785e747f85c37c161d
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-15 01:58:49 -0600

    feature/questions added answer validation

--- app.js
+++ app.js
@@ -16,11 +16,27 @@ const promptUser = () => {
         type: "input",
         name: "name",
         message: "what is your name",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your name.");
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub profile.");
+                return false;
+            }
+        }
       },
       {
         type: 'input',
@@ -45,12 +61,28 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a name for the project.");
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descInput => {
+                if (descInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of the project.");
+                    return false;
+                }
+            }
           },
           {
             type: 'checkbox',
@@ -61,7 +93,15 @@ Add a New Project
           {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a link to your GitHub.");
+                    return false;
+                }
+            }
           },
           {
             type: 'confirm',
