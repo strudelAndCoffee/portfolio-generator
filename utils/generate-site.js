54e16aac7e8f51fc9e0676fac784a05cffcf57e5
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-16 17:28:59 -0600

    created copy file function in generate-site.js

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -14,3 +14,23 @@ const writeFile = fileContent => {
         });
     });
 };
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile("./src/style.css", "./dist/style.css", err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File copied'
+            });
+        });
+    });
+};
+
+module.exports = {
+    writeFilr: writeFile,
+    copyFile: copyFile
+};
