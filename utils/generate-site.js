01994cb8f270af0e263113bf9aeb2eef6bfe48fc
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-16 15:06:22 -0600

    created generate-site.js and added write file function as promise handling

---
+++ utils/generate-site.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./dist/index.html", fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
